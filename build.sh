b4a36e21a777915968d9232778eba3e2ae2535d2
anton <asugolov@gmail.com>
2025-12-28 21:59:47 +0100

    build

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Convert all markdown
+# Step 1: call Pandoc to convert to Markdown
 for f in md/*.md; do
   date=$(head -n 6 $f | grep date | sed "s/\-//g" | grep -o "[0-9]\+")
-  pandoc "$f" -s --css=../../styles.css --toc -N -B template/nav.html --mathjax -o "posts/$date/index.html"
+  pandoc "$f" -s --css=../../styles.css --toc -N --toc-depth 4 -B template/nav.html --mathjax -o "posts/$date/index.html"
 done
 
-# Generate index
+# Step 2: built the list of posts
 python3 make.py
-
 cat index_header.html index_list.html index_footer.html > index.html
